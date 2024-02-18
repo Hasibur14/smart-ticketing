6c4caa0750677ce917f2ea6b79413ab1a354af19
hasibur14 <hasib141415@gmail.com>
2024-02-19 00:42:02 +0600

    added modal

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const allSeat = document.getElementsByClassName('seat');
+let totalSeat = 40;
 let count = 0;
 let totalPrice = 0;
 for (const seat of allSeat) {
     seat.addEventListener('click', function (e) {
 
+        totalSeat = totalSeat - 1;
         count = count + 1;
+
 
         const seatNo = e.target.innerText;
 
@@ -28,10 +31,12 @@ for (const seat of allSeat) {
 
         // calculated total price
         getTotalPrice("total-price", parseInt(price));
+
+
+        setInnerText('seat-count', totalSeat);
         setInnerText('cart-count', count);
     });
 };
-
 
 // total price
 function getTotalPrice(id, value) {
@@ -41,8 +46,59 @@ function getTotalPrice(id, value) {
     setInnerText('total-price', sum);
 };
 
+//grand total with coupon code
+const applyBtn = document.getElementById('apply-btn');
+applyBtn.addEventListener('click', function () {
 
-// Inner Text show
-function setInnerText(id, value) {
-    document.getElementById(id).innerText = value;
-}
+
+    const discountElement = document.getElementById('input-field').value;
+    // const couponCode1 = discountElement.split(' ').join('').toUpperCase();
+    // const couponCode2 = discountElement.split(' ').join('').toUpperCase();
+
+    //  const couponCode1 = discountElement.;
+    // const couponCode2 = discountElement.;
+
+    
+
+
+        if (couponCode1 === 'NEW15') {
+            //discount price calculation
+            const discountElement = document.getElementById('discountPrice1');
+            const discountAmount = totalPrice * 0.15;
+            discountElement.innerText = discountAmount.toFixed(2);
+
+            //rest total calculation
+            const restTotal = document.getElementById('total');
+            const restTotalPrice = totalPrice - discountAmount;
+            restTotal.innerText = restTotalPrice.toFixed(2);
+
+            document.getElementById('input-field').value = '';
+
+        }
+        else if(couponCode2 === 'Couple 20'){
+            const discountElement = document.getElementById('discountPrice');
+            const discountAmount = totalPrice * 0.2;
+            discountElement.innerText = discountAmount.toFixed(2);
+
+            //rest total calculation
+            const restTotal = document.getElementById('total');
+            const restTotalPrice = totalPrice - discountAmount;
+            restTotal.innerText = restTotalPrice.toFixed(2);
+
+            document.getElementById('input-field').value = '';
+        }
+    
+    else {
+        alert("please at least $200 buy");
+        document.getElementById('input-field').value = '';
+    }
+});
+
+
+
+    // Inner Text show
+    function setInnerText(id, value) {
+        document.getElementById(id).innerText = value;
+    };
+
+
