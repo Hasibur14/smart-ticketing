735f38b859422817cba4c76146ba38c688e4950d
hasibur14 <hasib141415@gmail.com>
2024-02-19 01:46:23 +0600

    responsive all repo

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const allSeat = document.getElementsByClassName('seat');
 let totalSeat = 40;
 let count = 0;
 let totalPrice = 0;
+
 for (const seat of allSeat) {
     seat.addEventListener('click', function (e) {
-
         totalSeat = totalSeat - 1;
-        count = count + 1;
+       
 
+        if(count.length === 4){
+            count = count + 1;
+        }
+        else{
+            alert("You Parches maximum 4 seat");
+        };
 
         const seatNo = e.target.innerText;
-
         const price = e.target.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText;
 
         const selectedSeatNo = document.getElementById('Selected-place-container');
@@ -26,79 +31,47 @@ for (const seat of allSeat) {
         li.appendChild(p2);
         li.appendChild(p3);
 
-
         selectedSeatNo.appendChild(li);
 
-        // calculated total price
         getTotalPrice("total-price", parseInt(price));
-
 
         setInnerText('seat-count', totalSeat);
         setInnerText('cart-count', count);
     });
-};
+}
 
-// total price
 function getTotalPrice(id, value) {
-    const totalPrice = document.getElementById(id).innerText;
-    const convertedTotalPrice = parseInt(totalPrice);
-    const sum = convertedTotalPrice + parseInt(value);
-    setInnerText('total-price', sum);
-};
+    const totalPriceElement = document.getElementById(id);
+    const totalPriceValue = parseInt(totalPriceElement.innerText);
+    const sum = totalPriceValue + value;
+    setInnerText(id, sum);
+}
 
-//grand total with coupon code
 const applyBtn = document.getElementById('apply-btn');
 applyBtn.addEventListener('click', function () {
-
-
     const discountElement = document.getElementById('input-field').value;
-    // const couponCode1 = discountElement.split(' ').join('').toUpperCase();
-    // const couponCode2 = discountElement.split(' ').join('').toUpperCase();
 
-    //  const couponCode1 = discountElement.;
-    // const couponCode2 = discountElement.;
-
-    
-
-
-        if (couponCode1 === 'NEW15') {
-            //discount price calculation
-            const discountElement = document.getElementById('discountPrice1');
+    if (count == 4) {
+        if (discountElement === 'NEW15') {
             const discountAmount = totalPrice * 0.15;
-            discountElement.innerText = discountAmount.toFixed(2);
-
-            //rest total calculation
-            const restTotal = document.getElementById('total');
-            const restTotalPrice = totalPrice - discountAmount;
-            restTotal.innerText = restTotalPrice.toFixed(2);
-
+            setInnerText('discountPrice1', discountAmount.toFixed(2));
+            setInnerText('total', (totalPrice - discountAmount));
             document.getElementById('input-field').value = '';
-
-        }
-        else if(couponCode2 === 'Couple 20'){
-            const discountElement = document.getElementById('discountPrice');
+        } else if (discountElement === 'Couple 20') {
             const discountAmount = totalPrice * 0.2;
-            discountElement.innerText = discountAmount.toFixed(2);
-
-            //rest total calculation
-            const restTotal = document.getElementById('total');
-            const restTotalPrice = totalPrice - discountAmount;
-            restTotal.innerText = restTotalPrice.toFixed(2);
-
+            setInnerText('discountPrice', discountAmount.toFixed(2));
+            setInnerText('total', (totalPrice - discountAmount));
+            document.getElementById('input-field').value = '';
+        } else {
+            alert("Invalid coupon code");
             document.getElementById('input-field').value = '';
         }
-    
-    else {
-        alert("please at least $200 buy");
+    } else {
+        alert("Please buy at least $200");
         document.getElementById('input-field').value = '';
     }
 });
 
-
-
-    // Inner Text show
-    function setInnerText(id, value) {
-        document.getElementById(id).innerText = value;
-    };
-
-
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
